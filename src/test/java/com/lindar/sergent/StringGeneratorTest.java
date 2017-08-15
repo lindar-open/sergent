79a9140cf75e4e7785d6d8646f6d3009d68fd398
Iulian Dafinoiu <cicorongho@yahoo.com>
2017-08-15 14:26:04 +0100

    public test methods

--- src/test/java/com/lindar/sergent/StringGeneratorTest.java
+++ src/test/java/com/lindar/sergent/StringGeneratorTest.java
@@ -4,38 +4,38 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-class StringGeneratorTest {
+public class StringGeneratorTest {
 
     @Before
-    void setUp() {
+    public void setUp() {
     }
 
     @Test
-    void randAlphabeticString() {
+    public void randAlphabeticString() {
         StringGenerator generator = SergentFactory.newInstance().stringGenerator();
         Assert.assertTrue("String contains more than just alphabetic chars", generator.alphabetic().randString().matches("[a-zA-Z]+"));
     }
 
     @Test
-    void randAlphaNumericString() {
+    public void randAlphaNumericString() {
         StringGenerator generator = SergentFactory.newInstance().stringGenerator();
         Assert.assertTrue("String contains more than just alphanumeric chars", generator.alphanumeric().randString().matches("[a-zA-Z1-9]+"));
     }
 
     @Test
-    void randNumericString() {
+    public void randNumericString() {
         StringGenerator generator = SergentFactory.newInstance().stringGenerator();
         Assert.assertTrue("String contains more than just numeric chars", generator.numeric().randString().matches("[1-9]+"));
     }
 
     @Test
-    void randUppercaseString() {
+    public void randUppercaseString() {
         StringGenerator generator = SergentFactory.newInstance().stringGenerator();
         Assert.assertTrue("String contains more than just uppercase chars", generator.uppercase().randString().matches("[A-Z]+"));
     }
 
     @Test
-    void randLowercaseString() {
+    public void randLowercaseString() {
         StringGenerator generator = SergentFactory.newInstance().stringGenerator();
         Assert.assertTrue("String contains more than just lowercase chars", generator.lowercase().randString().matches("[a-z]+"));
     }
