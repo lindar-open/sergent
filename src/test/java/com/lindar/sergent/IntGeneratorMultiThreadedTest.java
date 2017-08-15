7f69b2e199b63e94d6d6241d48eec37c1fdbf12f
Iulian Dafinoiu <cicorongho@yahoo.com>
2017-08-15 14:27:43 +0100

    register thread info in test

--- src/test/java/com/lindar/sergent/IntGeneratorMultiThreadedTest.java
+++ src/test/java/com/lindar/sergent/IntGeneratorMultiThreadedTest.java
@@ -7,31 +7,31 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import java.util.List;
 import java.util.stream.IntStream;
 
 @RunWith(ConcurrentTestRunner.class)
 public class IntGeneratorMultiThreadedTest {
-    private static final int SIZE_CHECK = 10_001;
+    private static final int SIZE_CHECK = 20_001;
     private final static int THREAD_COUNT = 4;
 
     @Before
     public void setUp() {
     }
 
-    @Test
-    @ThreadCount(THREAD_COUNT)
-    public void randMultiThreadedBigIntList() {
-        List<Integer> list = SergentFactory.newInstance().listGenerator().ofSize(SIZE_CHECK).randIntegers();
-        Assert.assertTrue("Not all numbers have been generated", list.size() == SIZE_CHECK);
-    }
+//    @Test
+//    @ThreadCount(THREAD_COUNT)
+//    public void randMultiThreadedBigIntList() {
+//        List<Integer> list = SergentFactory.newInstance().listGenerator().ofSize(SIZE_CHECK).randIntegers();
+//        Assert.assertTrue("Not all numbers have been generated", list.size() == SIZE_CHECK);
+//    }
 
     @Test
     @ThreadCount(THREAD_COUNT)
     public void randMultiThreadedSingleInt() {
-        System.out.println("Running thread : " + SergentFactory.newInstance().intGenerator().randInt());
+        Sergent sergent = SergentFactory.newInstance();
+        System.out.println("Running thread : " + sergent.intGenerator().randInt() + " with provider ID: " + sergent.randomProviderId);
         IntStream.rangeClosed(0, SIZE_CHECK).forEach(i -> {
-            Integer number = SergentFactory.newInstance().intGenerator().randInt();
+            Integer number = sergent.intGenerator().randInt();
             Assert.assertNotNull("Failed to generate random number", number);
         });
     }
